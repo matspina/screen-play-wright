edf769f656ca5a71c94e4449808025d928b889ed
Matheus Spina Polito <mat.spina@gmail.com>
2025-09-01 13:11:46 -0300

    Updating visual regression snapshots.

--- src/page-objects/demo-site/sample-page-test.ts
+++ src/page-objects/demo-site/sample-page-test.ts
@@ -18,7 +18,7 @@ export class SamplePageTest {
 			TEXT_AREA_COMMENTS: 'textarea[name*=comment]',
 			CONTACT_FORM_SUCCERSS_HEADER: '#contact-form-success-header',
 			FORM_ERROR_WRAPPER: '.form-errors',
-			PAGE_HEADER_CONTAINER: '.full_container_page_title'
+			PAGE_HEADER_CONTAINER: '.full_container_page_title > div > div'
 		}
 	}
 }
